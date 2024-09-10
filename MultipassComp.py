6bd686e5ad40e736f030dc9309d5c10b10a9fc1b
cesarioni <cesarioni@hotmail.com>
2024-09-10 15:41:43 +0200

    Update MultipassComp.py
    lg and passe setup separated into different functions

--- MultipassComp.py
+++ MultipassComp.py
@@ -14,19 +14,24 @@ C = bpy.context
 
 def setupRender():
     
-    remove_compositor_nodes()
+    
     scene = bpy.context.scene
     scene.use_nodes = True
-    counter=0
     offset = 5500
+    remove_compositor_nodes()
+    thePasses = setupRenderPasses()
     FileBaseName = getFileBaseName()
     
-    for viewLayer in bpy.context.scene.view_layers:
-        #compositor_node_tree = scene.node_tree
+    for counter, viewLayer in enumerate(bpy.context.scene.view_layers):
         render_layers_node = scene.node_tree.nodes.new(type="CompositorNodeRLayers")
         render_layers_node.layer= viewLayer.name
         render_layers_node.location.y=offset*counter
-
+        #Create routes for passes
+        routesPassArray = createPassReroute(thePasses, 3400, (offset*counter)-50, viewLayer.name)
+        passesOperation(routesPassArray, viewLayer.name)
+        #passesAlphasOutputs(routesPassArray)
+        #Create routes for lightgroups
+        routesLGArray = createLGReroute(routesPassArray[1], 3400, (offset*counter)-50, viewLayer.name)
         #Create exr outputs
         file_output_node = createOutputs(FileBaseName + "_" + viewLayer.name, viewLayer.name, 1000, (offset*counter)-150)
         # Reset File Output node layer slots and match them to enabled Render Layers
@@ -41,104 +46,100 @@ def setupRender():
         # Connect the sockets between the two nodes
         for i, socket in enumerate([s for s in render_layers_node.outputs if s.enabled]):
             bpy.context.scene.node_tree.links.new(file_output_node.inputs[i], socket)
-            
         createImageNode(2000, (offset*counter)-150)
-        createReroutes(3400, (offset*counter)-50, viewLayer.name)
-        counter+=1
         
 def setupRenderPasses():
     for theViewLayer in bpy.context.scene.view_layers:
+        passes = []
+        passes.append("NoisyImage")
+        passes.append("Alpha")
+        theViewLayer.use_pass_z = True
+        passes.append("ZDepth")
         theViewLayer.use_pass_mist = True
+        passes.append("Mist")
         theViewLayer.use_pass_diffuse_direct = True
+        passes.append("DiffDir")
         theViewLayer.use_pass_diffuse_indirect = True
+        passes.append("DiffInd")
         theViewLayer.use_pass_diffuse_color = True
+        passes.append("DiffCol")
         theViewLayer.use_pass_glossy_direct = True
+        passes.append("GlossDir")
         theViewLayer.use_pass_glossy_indirect = True
+        passes.append("GlossInd")
         theViewLayer.use_pass_glossy_color = True
+        passes.append("GlossCol")
         theViewLayer.use_pass_transmission_direct = True
+        passes.append("TransDir")
         theViewLayer.use_pass_transmission_indirect = True
+        passes.append("TransInd")
         theViewLayer.use_pass_transmission_color = True
+        passes.append("TransCol")
+        return passes
     
 def createImageNode(xPos, yPos):
-    #print (bpy.context.scene.view_layers["Environment"].RenderPasses)
     imageNode = bpy.context.scene.node_tree.nodes.new(type="CompositorNodeImage")
     positionNodes(imageNode, xPos, yPos)
     return imageNode
 
-def createPassReroute(xPos, yPos, viewLayerName, label, loopVar = 1):
+def createPassReroute(thePasses, xPos, yPos, viewLayerName):
+    reroutesArray=[]
     ofsset=150
-    rerouteMist = createDot(label,xPos, yPos-ofsset*loopVar)
+    for count, thePass in enumerate(thePasses):
+        currentReroute = createDot(thePass ,xPos, yPos-ofsset*count)
+        currentSwitch = createSwitch(xPos-500, yPos-ofsset*count)
+        reroutesArray.append(currentReroute)
+        bpy.context.scene.node_tree.links.new(currentSwitch.outputs[0], currentReroute.inputs[0])
+    return reroutesArray
 
-def createReroutes(xPos, yPos, viewLayerName):
-    ofsset=150
-    print(bpy.context.scene.view_layers[viewLayerName].aovs)
-    
-    rerouteMist = createDot("Mist",xPos, yPos-ofsset*1)
-    rerouteDiffDir = createDot("DiffDir",xPos, yPos-ofsset*2)
-    rerouteDiffInd = createDot("DiffInd",xPos, yPos-ofsset*3)
-    rerouteDiffCol = createDot("DiffCol",xPos, yPos-ofsset*4)
-    rerouteGlossDir = createDot("GlossDir",xPos, yPos-ofsset*5)
-    rerouteGlossInd = createDot("GlossInd",xPos, yPos-ofsset*6)
-    rerouteGlossCol = createDot("GlossCol",xPos, yPos-ofsset*7)
-    rerouteTransDir = createDot("TransDir",xPos, yPos-ofsset*8)
-    rerouteTransInd = createDot("TransInd",xPos, yPos-ofsset*9)
-    rerouteTransCol = createDot("TransCol",xPos, yPos-ofsset*10)
-    rerouteAlpha = createDot("Alpha",xPos, yPos-ofsset*11)
-    rerouteImage = createDot("NoisyImage",xPos, yPos-ofsset*12)
-    rerouteZ = createDot("ZDepth",xPos, yPos-ofsset*13)
-    
-
-    switchArray=[]
-    for i in range (13):
-        currentSwitch = createSwitch(xPos-500, yPos-ofsset*i)
-        switchArray.append(currentSwitch)
-        
-    bpy.context.scene.node_tree.links.new(switchArray[0].outputs[0], rerouteMist.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[1].outputs[0], rerouteDiffDir.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[2].outputs[0], rerouteDiffInd.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[3].outputs[0], rerouteDiffCol.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[4].outputs[0], rerouteGlossDir.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[5].outputs[0], rerouteGlossInd.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[6].outputs[0], rerouteGlossCol.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[7].outputs[0], rerouteTransDir.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[8].outputs[0], rerouteTransInd.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[9].outputs[0], rerouteTransCol.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[10].outputs[0], rerouteAlpha.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[11].outputs[0], rerouteImage.inputs[0])
-    bpy.context.scene.node_tree.links.new(switchArray[12].outputs[0], rerouteZ.inputs[0])
-    
-    lightCombined = combineElements(rerouteDiffDir, rerouteDiffInd, 'ADD', 1)
-    diffLight = combineElements(lightCombined, rerouteDiffCol, 'MULTIPLY', 2)
-    
-    glossyCombined = combineElements(rerouteGlossDir, rerouteGlossInd, 'ADD', 1)
-    diffGlossy = combineElements(glossyCombined , rerouteGlossCol, 'MULTIPLY', 2)
-    
-    transCombined = combineElements(rerouteTransDir, rerouteTransInd, 'ADD', 1)
-    diffTrans = combineElements(transCombined , rerouteTransCol, 'MULTIPLY', 2)
-    
+def passesOperation(PassReroute, viewLayerName):
+    lightCombined = combineElements(PassReroute[4], PassReroute[5], 'ADD', 1)
+    diffLight = combineElements(lightCombined, PassReroute[6], 'MULTIPLY', 2)
+    glossyCombined = combineElements(PassReroute[7], PassReroute[8], 'ADD', 1)
+    diffGlossy = combineElements(glossyCombined , PassReroute[9], 'MULTIPLY', 2)
+    transCombined = combineElements(PassReroute[10], PassReroute[11], 'ADD', 1)
+    diffTrans = combineElements(transCombined , PassReroute[12], 'MULTIPLY', 2)
     transLightCombined = combineElements(diffLight, diffTrans, 'ADD', 3)
-    
     totalGlossyCombined = combineElements(diffGlossy, transLightCombined, 'ADD', 3.5)
+    addMist = combineElements(totalGlossyCombined, PassReroute[3], 'ADD', 3)
+    copyAlpha_passes = getAlpha(addMist, PassReroute[1])
+
+    #Copy the alpha to all passes
+    diffAlpha = getAlpha(PassReroute[6], PassReroute[1], 30)
+    lightAlpha = getAlpha(lightCombined, PassReroute[1], 30)
+    glossyAlpha = getAlpha(diffGlossy, PassReroute[1], 30)
+    transmitionAlpha = getAlpha(diffTrans, PassReroute[1], 30)
+    mistAlpha = getAlpha(PassReroute[3], PassReroute[1], 30)
     
-    addMist = combineElements(totalGlossyCombined, rerouteMist, 'ADD', 3)
-      
-    copyAlpha_passes = getAlpha(addMist, rerouteAlpha)
+    #creating the output node and connecting to the passes with alpha
     
-    ####light group setup
-    numLG = len(bpy.context.scene.view_layers[viewLayerName].lightgroups)
+    file_output_Comp_Passes = createOutputsB(copyAlpha_passes,viewLayerName, 'OPEN_EXR', 'comp')
+
+    file_output_Diffuse = createOutputsB(diffAlpha,viewLayerName, 'OPEN_EXR', 'Diffuse')
+    file_output_Light = createOutputsB(lightAlpha ,viewLayerName, 'OPEN_EXR', 'Lighting')
+    file_output_Glossy = createOutputsB(glossyAlpha,viewLayerName, 'OPEN_EXR', 'Specular')
+    file_output_Transmition = createOutputsB(transmitionAlpha,viewLayerName, 'OPEN_EXR', 'Transmition')
+    file_output_Mist = createOutputsB(mistAlpha,viewLayerName, 'OPEN_EXR', 'Mist')
+    file_output_Image = createOutputsB(PassReroute[0],viewLayerName, 'OPEN_EXR', 'Image')
+
+
+
+def createLGReroute(theAlpha, xPos, yPos, viewLayerName):
     knotsArray= []
     addArray = []
+    ofsset=150
+    separation = 15
+    numLG = len(bpy.context.scene.view_layers[viewLayerName].lightgroups)
     if numLG > 0:##checks if lightgroups exists in curretnview layer
         ##createdots and addMixNodes
         for i in range(numLG):
-            SwitchNode = createSwitch(xPos-500, yPos-ofsset*(15+i))
-            rerouteLG = createDot("LG"+str(i),xPos, yPos-ofsset*(15+i))
+            SwitchNode = createSwitch(xPos-500, yPos-ofsset*(separation+i))
+            rerouteLG = createDot("LG"+str(i),xPos, yPos-ofsset*(separation+i))
             bpy.context.scene.node_tree.links.new(SwitchNode.outputs[0], rerouteLG.inputs[0])
             knotsArray.append(rerouteLG)
             if i < numLG-1:
                 AddNode = createMixNode('ADD', rerouteLG.location.x+50+(200*i), rerouteLG.location.y)
-                addArray.append(AddNode)
-        
+                addArray.append(AddNode)     
         ##Connect nodes    
         for i in range(numLG):
             if i==0:
@@ -151,30 +152,9 @@ def createReroutes(xPos, yPos, viewLayerName):
                 ##Connect add nodes in sequence
                 connectAdds(addArray[i],addArray[i+1],0,1)
                 
-        copyAlpha_LG = getAlpha(addArray[numLG-2], rerouteAlpha)
-        file_output_Comp_LG = createOutputsB(copyAlpha_LG,viewLayerName, 'OPEN_EXR', 'compLG')         
-        
-
-
-    ###OUTPUTS
-    #Copy the alpha to all passes
-    diffAlpha = getAlpha(rerouteDiffCol, rerouteAlpha, 30)
-    lightAlpha = getAlpha(lightCombined, rerouteAlpha, 30)
-    glossyAlpha = getAlpha(diffGlossy, rerouteAlpha, 30)
-    transmitionAlpha = getAlpha(diffTrans, rerouteAlpha, 30)
-    mistAlpha = getAlpha(rerouteMist, rerouteAlpha, 30)
-    
-    #creating the output node and connecting to the passes with alpha
-    
-    ############file_output_Comp_LG = createOutputsB(copyAlpha_LG,viewLayerName, 'OPEN_EXR', 'comp')
-    file_output_Comp_Passes = createOutputsB(copyAlpha_passes,viewLayerName, 'OPEN_EXR', 'comp')
-    
-    file_output_Diffuse = createOutputsB(diffAlpha,viewLayerName, 'OPEN_EXR', 'Diffuse')
-    file_output_Light = createOutputsB(lightAlpha ,viewLayerName, 'OPEN_EXR', 'Lighting')
-    file_output_Glossy = createOutputsB(glossyAlpha,viewLayerName, 'OPEN_EXR', 'Specular')
-    file_output_Transmition = createOutputsB(transmitionAlpha,viewLayerName, 'OPEN_EXR', 'Transmition')
-    file_output_Mist = createOutputsB(mistAlpha,viewLayerName, 'OPEN_EXR', 'Mist')
-    file_output_Image = createOutputsB(rerouteImage,viewLayerName, 'OPEN_EXR', 'Image')
+        copyAlpha_LG = getAlpha(addArray[numLG-2], theAlpha)
+        file_output_Comp_LG = createOutputsB(copyAlpha_LG,viewLayerName, 'OPEN_EXR', 'compLG')    
+    return knotsArray
     
 def connectAdds(element1, element2, output1, input1):
     bpy.context.scene.node_tree.links.new(element1.outputs[output1], element2.inputs[input1])
@@ -257,5 +237,5 @@ def remove_compositor_nodes():
     bpy.context.scene.use_nodes = True
     bpy.context.scene.node_tree.nodes.clear()
  
-setupRenderPasses()        
+        
 setupRender()
